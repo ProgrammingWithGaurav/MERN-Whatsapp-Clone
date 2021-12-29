abca0335b9e5121ab2102f02003eab00d4c241a1
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-29 09:16:18 +0530

    Initial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# MERN Whatsapp Clone
+- live site : https://mern-what-sapp.web.app/
+
+# Tech stack used
+- Mongoose
+- Mongodb Atlas
+- ExpressJs
+- ReactJS
+- Nodejs
+- firebase for authentication and hosting
---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA36UeaAy7aJ-fsDoJMMd2OHopxe9lr0ew",
+    authDomain: "reactprojects-12bbe.firebaseapp.com",
+    databaseURL: "https://reactprojects-12bbe-default-rtdb.firebaseio.com",
+    projectId: "reactprojects-12bbe",
+    storageBucket: "reactprojects-12bbe.appspot.com",
+    messagingSenderId: "742066518833",
+    appId: "1:742066518833:web:5a333f20cb9463b79d4e10",
+    measurementId: "G-6RT762JQN0"
+  };
