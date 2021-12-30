5597fb8f997f6475502c925d7bb430aacea64475
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-30 14:53:31 +0530

    App Successfully depployed 🍎

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://mern-whatsapp-api.herokuapp.com',
 })
 
 export default instance
